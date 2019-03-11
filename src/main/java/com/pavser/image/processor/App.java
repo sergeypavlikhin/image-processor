06bae09c907dc323902f98f8b8e0bf4c22af247a
sergeypavlikhin <sergey.pavlikhin@inbox.ru>
2019-03-11 18:59:15 +0300

    init

--- src/main/java/com/pavser/image/processor/App.java
+++ src/main/java/com/pavser/image/processor/App.java
@@ -2,6 +2,14 @@ package com.pavser.image.processor;
 
 import org.apache.commons.cli.*;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * https://stackoverflow.com/questions/9131678/convert-a-rgb-image-to-grayscale-image-reducing-the-memory-in-java
  * https://www.baeldung.com/java-images
@@ -18,10 +26,6 @@ public class App {
         input.setRequired(true);
         options.addOption(input);
 
-        Option output = new Option("o", "output", true, "output file");
-        output.setRequired(true);
-        options.addOption(output);
-
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd = null;
@@ -36,10 +40,31 @@ public class App {
         }
 
         String inputFilePath = cmd.getOptionValue("input");
-        String outputFilePath = cmd.getOptionValue("output");
+
+
+        try {
+            URI uri = new URI(inputFilePath);
+            File inputFile = new File(uri);
+            BufferedImage inputImage = ImageIO.read(inputFile);
+            BufferedImage outputImage = new BufferedImage(100,100, BufferedImage.TYPE_BYTE_GRAY);
+            Graphics2D g2d = outputImage.createGraphics();
+            g2d.drawImage(inputImage, 0, 0, 100, 100, null);
+            g2d.dispose();
+            String formatName = inputFilePath.substring(inputFilePath
+                    .lastIndexOf(".") + 1);
+            File output1 = new File("res_1." + formatName);
+            if (!output1.exists()) {
+                output1.createNewFile();
+            }
+            ImageIO.write(outputImage, formatName, output1);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         System.out.println(inputFilePath);
-        System.out.println(outputFilePath);
 
     }
 }
